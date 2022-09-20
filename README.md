512cfe25ed3b3e519344c572c06883732d07516a
unknown <lyf0214white@gmail.com>
2022-09-20 18:05:43 +0800

    update readme

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 WebRTC - P2P
 
-- [x] chat(基本依据peer.js官网示例)
+- [x] Words chat(基本依据peer.js官网示例)
+- [x] Video chat([reference](https://github.com/haixiangyan/react-p2p-chatroom))
+- [ ] Files exchange
--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
   <router-link to="/receiver" class="function_router">接收端</router-link>
 
-  <router-link to="/exchangeroom" class="function_router">交换端</router-link>
+  <router-link to="/exchangeroom" class="function_router">二合一端</router-link>
   <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
